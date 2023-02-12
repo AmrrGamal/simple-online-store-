03a19ec35760a8d181b432342fdeaff8fcd0024a
Amr Gamal <113727947+AmrrGamal@users.noreply.github.com>
2023-02-12 21:38:45 +0200

    Add files via upload

---
+++ online store/car.php
@@ -0,0 +1,110 @@
+<?php
+
+include 'config.php';
+session_start();
+$user_id = $_SESSION['user_id'];
+
+
+if(isset($_POST['update_cart'])){
+    $update_quantity = $_POST['cart_quantity'];
+    $update_id = $_POST['cart_id'];
+    mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
+    $message[] = 'تم تحديث كمية سلة التسوق بنجاح!';
+ }
+ 
+ if(isset($_GET['remove'])){
+    $remove_id = $_GET['remove'];
+    mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
+    header('location:index.php');
+ }
+   
+ if(isset($_GET['delete_all'])){
+    mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+    header('location:index.php');
+ }
+
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>عربة التسوق</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div  class="container">   <!-- السر -->
+
+<div class="shopping-cart">
+
+   <h1 class="heading"> عربة التسوق</h1>
+
+   <table>
+      <thead>
+         <th>الصورة</th>
+         <th>الاسم</th>
+         <th>السعر</th>
+         <th>العدد</th>
+         <th>السعر الكلي</th>
+         <th>العمل</th>
+      </thead>
+      <tbody>
+         
+      <?php
+         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+         $grand_total = 0;
+         if(mysqli_num_rows($cart_query) > 0){
+            while($fetch_cart = mysqli_fetch_assoc($cart_query)){
+      ?>
+      
+         <tr>
+            <td><img src="admin/<?php echo $fetch_cart['image']; ?>" height="75" alt=""></td>
+            <td><?php echo $fetch_cart['name']; ?></td>
+            <td><?php echo $fetch_cart['price']; ?>$ </td>
+            <td>
+               <form action="" method="post">
+                  <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
+                  <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
+                  <input type="submit" name="update_cart" value="تعديل" class="option-btn">
+               </form>
+            </td>
+            <td><?php echo $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?>$</td>
+            <td><a href="index.php?remove=<?php echo $fetch_cart['id']; ?>" class="delete-btn" onclick="return confirm('إزالة العنصر من سلة التسوق؟');">حذف</a></td>
+         </tr>
+      <?php
+         $grand_total += $sub_total;
+            }
+         }else{
+            echo '<tr><td style="padding:20px; text-transform:capitalize;" colspan="6">العربة فارغة</td></tr>';
+         }
+      ?>
+      <tr class="table-bottom">
+         <td colspan="4">المبلغ الإجمالي :</td>
+         <td><?php echo $grand_total; ?>$</td>
+         <td><a href="index.php?delete_all" onclick="return confirm('حذف كل المنتجات من العربة?');" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">حذف الكل</a></td>
+      </tr>
+   </tbody>
+   </table>
+
+
+
+</div>
+      
+</div>
+
+
+</body>
+</html>
